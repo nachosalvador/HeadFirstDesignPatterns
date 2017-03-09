4effb671854d858383d1b23e043e688090c285be
Nacho Salvador <nachosalvador83@gmail.com>
2017-03-09 18:41:09 +0100

    Add break lines

--- Factory/FactoryMethod/PizzaTestDrive.php
+++ Factory/FactoryMethod/PizzaTestDrive.php
@@ -9,25 +9,25 @@ $NYStore = new NYPizzaStore();
 $chicagoStore = new ChicagoPizzaStore();
 
 $pizza = $NYStore->orderPizza('cheese');
-print('Ethan ordered a ' . $pizza->getName() . PHP_EOL);
+print('Ethan ordered a ' . $pizza->getName() . "\n\n");
 
 $pizza = $chicagoStore->orderPizza('cheese');
-print('Joel ordered a ' . $pizza->getName() . PHP_EOL);
+print('Joel ordered a ' . $pizza->getName() . "\n\n");
 
 $pizza = $NYStore->orderPizza('clam');
-print('Ethan ordered a ' . $pizza->getName() . PHP_EOL);
+print('Ethan ordered a ' . $pizza->getName() . "\n\n");
 
 $pizza = $chicagoStore->orderPizza('clam');
-print('Joel ordered a ' . $pizza->getName() . PHP_EOL);
+print('Joel ordered a ' . $pizza->getName() . "\n\n");
 
 $pizza = $NYStore->orderPizza('pepperoni');
-print('Ethan ordered a ' . $pizza->getName() . PHP_EOL);
+print('Ethan ordered a ' . $pizza->getName() . "\n\n");
 
 $pizza = $chicagoStore->orderPizza('pepperoni');
-print('Joel ordered a ' . $pizza->getName() . PHP_EOL);
+print('Joel ordered a ' . $pizza->getName() . "\n\n");
 
 $pizza = $NYStore->orderPizza('veggie');
-print('Ethan ordered a ' . $pizza->getName() . PHP_EOL);
+print('Ethan ordered a ' . $pizza->getName() . "\n\n");
 
 $pizza = $chicagoStore->orderPizza('veggie');
-print('Joel ordered a ' . $pizza->getName() . PHP_EOL);
+print('Joel ordered a ' . $pizza->getName() . "\n\n");
